a1168b3d4ac2ff50245936618686e51b3ef3762d
Jesus Marron <jesusmarron7@gmail.com>
2020-03-25 01:19:58 -0500

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tic-tac-toe App
 >Simple tic-tac-toe app made with Angular
+>DEMO: https://tic-tac-toe-b822c.firebaseapp.com/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
     NbEvaIconsModule,
-    NbButtonModule
+    NbButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
